31201be60a8aeddaf994f46abb43916c752e5be6
James Sandford Smith <jesandfordsmith@gmail.com>
2023-06-29 23:28:45 +0100

    edit working

--- JS/editTask.js
+++ JS/editTask.js
@@ -8,46 +8,45 @@ export const editTask = () => {
 function openEditor() {
     const parent = event.target.parentElement;
     const label = parent.querySelector(".checkbox-label");
+    // buttons
     const delBtn = parent.querySelector(".delbtn");
+    const editBtn = event.target;
+    const acceptEdit = editBtn.cloneNode();
+    // button swap
+    acceptEdit.innerHTML = "&#10003";
+    acceptEdit.classList.toggle("editbtn");
+    parent.replaceChild(acceptEdit,editBtn);
+    const cancelBtn = delBtn.cloneNode();
+    cancelBtn.innerHTML = "&#10060";
+    cancelBtn.classList.add("canceledit");
+    cancelBtn.classList.toggle("delbtn");
+    parent.replaceChild(cancelBtn,delBtn);
 
+    // define text input
     const editInput = document.createElement("input");
     editInput.classList.add("edittext");
     editInput.type = "text";
     editInput.value = label.textContent;
     editInput.placeholder = label.textContent;
+    // swap in text input
     parent.replaceChild(editInput,label);
 
-    event.target.innerHTML = "&#10003";
-    event.target.classList.toggle("editbtn")  // this should not be needed but because im accessing this file with setInterval the eventlistener gets added back automatically can remove once integrated in script properly
-    const cancelBtn = delBtn.cloneNode();
-    cancelBtn.innerHTML = "&#10060";
-    cancelBtn.classList.add("canceledit");
-    cancelBtn.classList.toggle("delbtn");
-    // delBtn.innerHTML = "&#10060";
-    parent.replaceChild(cancelBtn,delBtn);
-    event.target.removeEventListener("click",openEditor);
-    event.target.addEventListener("click", e => {
-        closeEditor(true);
+    const btnArr = [[acceptEdit,editBtn],[cancelBtn,delBtn]];
+    acceptEdit.addEventListener("click", e => {
+        closeEditor(true,label,btnArr);
     });
     cancelBtn.addEventListener("click", e => {
-        closeEditor(false);
+        closeEditor(false,label,btnArr);
     })
 }
 
-function closeEditor(acceptChanges) {
+function closeEditor(acceptChanges,label,btnArr) {
     const parent = event.target.parentElement;
-    const editInput = parent.querySelector(".edittext")
-    console.log("close edit",{editInput});
-    const label = document.createElement("label");
-    label.htmlFor = "checkbox";
-    label.className = "checkbox-label";
+    const editInput = parent.querySelector(".edittext")    
 
     if (acceptChanges) label.textContent = editInput.value;
     else label.textContent = editInput.placeholder;
 
     parent.replaceChild(label,editInput);
-    event.target.innerHTML = "&#9998";
-    event.target.classList.toggle("editbtn")  // this should not be needed but because im accessing this file with setInterval the eventlistener gets added back automatically can remove once integrated in script properly
-    event.target.removeEventListener("click",closeEditor);
-    event.target.addEventListener("click",openEditor)
+    btnArr.forEach(e => parent.replaceChild(e[1],e[0]))
 }
