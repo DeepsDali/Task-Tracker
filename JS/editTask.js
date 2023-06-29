e315499f2630d5e507027db5169d5a314222f20b
James Sandford Smith <jesandfordsmith@gmail.com>
2023-06-29 16:00:46 +0100

    started making functionality for edit editButton edit button now finds parent element and text content of sibling element

---
+++ JS/editTask.js
@@ -0,0 +1,12 @@
+export const editTask = () => {
+    const editBtns = document.getElementsByClassName("editbtn");  // creates array from all element nodes with editbtn class
+    [...editBtns].forEach(btn => // spread editbtns array so we can access each node
+        btn.addEventListener("click", //adds event listener to each node
+        openEditor));  // removes parent element (taking children with it) on button press
+}
+function openEditor() {
+    const parent = event.target.parentElement;
+    const label = parent.querySelector(".checkbox-label")
+    console.log("here",{parent},label.textContent)
+    
+}
