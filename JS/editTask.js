90e5a68cc53d5242d6ce466b37dbb335364afba7
James Sandford Smith <jesandfordsmith@gmail.com>
2023-06-29 16:43:00 +0100

    can now confirm edit

--- JS/editTask.js
+++ JS/editTask.js
@@ -4,12 +4,33 @@ export const editTask = () => {
         btn.addEventListener("click", //adds event listener to each node
         openEditor));  // removes parent element (taking children with it) on button press
 }
+
 function openEditor() {
     const parent = event.target.parentElement;
     const label = parent.querySelector(".checkbox-label")
-    console.log("here",{parent},label.textContent)
+    console.log("edit",{parent},label.textContent)
     const editInput = document.createElement("input");
+    editInput.classList.add("edittext")
     editInput.type = "text";
     editInput.value = label.textContent;
     parent.replaceChild(editInput,label);
+    event.target.innerHTML = "&#10003";
+    event.target.classList.toggle("editbtn")
+    event.target.removeEventListener("click",openEditor);
+    event.target.addEventListener("click",closeEditor)
+}
+
+function closeEditor() {
+    const parent = event.target.parentElement;
+    const editInput = parent.querySelector(".edittext")
+    console.log("close edit",{editInput});
+    const label = document.createElement("label");
+    label.htmlFor = "checkbox";
+    label.className = "checkbox-label";
+    label.textContent = editInput.value;
+    parent.replaceChild(label,editInput);
+    event.target.innerHTML = "&#9998";
+    event.target.classList.toggle("editbtn")
+    event.target.removeEventListener("click",closeEditor);
+    event.target.addEventListener("click",openEditor)
 }
