6ea69382d36a7c9e17b4311a986148f5b4ab8ea8
James Sandford Smith <jesandfordsmith@gmail.com>
2023-06-29 23:10:42 +0100

    cancel button semiworking
    shares function with accept changes so need to adjust function to accomodate

--- JS/editTask.js
+++ JS/editTask.js
@@ -7,27 +7,44 @@ export const editTask = () => {
 
 function openEditor() {
     const parent = event.target.parentElement;
-    const label = parent.querySelector(".checkbox-label")
-    console.log("edit",{parent},label.textContent)
+    const label = parent.querySelector(".checkbox-label");
+    const delBtn = parent.querySelector(".delbtn");
+
     const editInput = document.createElement("input");
-    editInput.classList.add("edittext")
+    editInput.classList.add("edittext");
     editInput.type = "text";
     editInput.value = label.textContent;
+    editInput.placeholder = label.textContent;
     parent.replaceChild(editInput,label);
+
     event.target.innerHTML = "&#10003";
     event.target.classList.toggle("editbtn")  // this should not be needed but because im accessing this file with setInterval the eventlistener gets added back automatically can remove once integrated in script properly
+    const cancelBtn = delBtn.cloneNode();
+    cancelBtn.innerHTML = "&#10060";
+    cancelBtn.classList.add("canceledit");
+    cancelBtn.classList.toggle("delbtn");
+    // delBtn.innerHTML = "&#10060";
+    parent.replaceChild(cancelBtn,delBtn);
     event.target.removeEventListener("click",openEditor);
-    event.target.addEventListener("click",closeEditor)
+    event.target.addEventListener("click", e => {
+        closeEditor(true);
+    });
+    cancelBtn.addEventListener("click", e => {
+        closeEditor(false);
+    })
 }
 
-function closeEditor() {
+function closeEditor(acceptChanges) {
     const parent = event.target.parentElement;
     const editInput = parent.querySelector(".edittext")
     console.log("close edit",{editInput});
     const label = document.createElement("label");
     label.htmlFor = "checkbox";
     label.className = "checkbox-label";
-    label.textContent = editInput.value;
+
+    if (acceptChanges) label.textContent = editInput.value;
+    else label.textContent = editInput.placeholder;
+
     parent.replaceChild(label,editInput);
     event.target.innerHTML = "&#9998";
     event.target.classList.toggle("editbtn")  // this should not be needed but because im accessing this file with setInterval the eventlistener gets added back automatically can remove once integrated in script properly
