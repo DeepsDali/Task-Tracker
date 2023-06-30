0a29cc215a1cd77043f1ba7aad3381eb71b4bdc6
JamesESS <60142087+JamesESS@users.noreply.github.com>
2023-06-30 14:27:58 +0100

    delete button test

--- JS/Tests/tests.js
+++ JS/Tests/tests.js
@@ -1,4 +1,5 @@
 import { addTask } from "../addTask.js";
+import { removeTask } from "../deleteTask.js";
 
 // addTask Test
 
@@ -9,7 +10,7 @@ test("Add button creates a new task on the Task List", () => {
   const taskItem = taskList.querySelector(".task-item");
   const label = taskItem.querySelector("label");
   console.log(label);
-  taskItem.style.display = "none";
+  // taskItem.style.display = "none";
   equal(
     label.textContent,
     "Wash the car",
@@ -19,5 +20,23 @@ test("Add button creates a new task on the Task List", () => {
 });
 
 // removeTask Test
+
+test("removeTask removes the parent element of button when pressed", () => {
+  const textString = "delete button test"
+  const taskList = document.getElementById("task-list");
+  document.getElementById("addTask").value = textString;
+  addTask();  // adds new task to task list
+  const deleteDiv = [...document.getElementsByTagName("label")].filter(label => label.innerText == textString)[0].parentElement;  // find the new task div
+  const containerElement = deleteDiv.parentElement; // get parent of new task
+  const elementCount = containerElement.childElementCount; // count children
+  removeTask(); // initialise remove task
+  deleteDiv.querySelector(".delbtn").click();  // simulate click on delete button
+  const elementCountFinal = containerElement.childElementCount // get updated count of children
+  equal(  // ensure new count is one less than old count
+    elementCountFinal, 
+    elementCount - 1,
+    `expected element count of ${elementCount-1}, received ${elementCountFinal}`
+  )
+});
 // editTask Test
 // filterTask Test
