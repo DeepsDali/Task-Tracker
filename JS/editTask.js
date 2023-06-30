f2ca8178a00a04c69a5b8b9e214c812b11c9b8bd
James Sandford Smith <jesandfordsmith@gmail.com>
2023-06-30 09:16:07 +0100

    refactored whole file
    have added element creater functions should have on general prupose one for whole project ideally

--- JS/editTask.js
+++ JS/editTask.js
@@ -4,45 +4,51 @@ export const editTask = () => {
         btn.addEventListener("click", //adds event listener to each node
         openEditor));  // removes parent element (taking children with it) on button press
 }
-
-function openEditor() {  // opens text editor and changes buttons // could do with refactor fair bit of repetition
+// opens text editor and changes buttons to text editor relevant ones
+function openEditor() {
     const parent = event.target.parentElement;
     const label = parent.querySelector(".checkbox-label");
-    // buttons
     const delBtn = parent.querySelector(".delbtn");
     const editBtn = event.target;
-    const acceptEdit = editBtn.cloneNode();
-    // button swap
-    acceptEdit.innerHTML = "&#10003";
-    acceptEdit.classList.toggle("editbtn");
-    const cancelBtn = delBtn.cloneNode();
-    cancelBtn.innerHTML = "&#10060";
-    cancelBtn.classList.toggle("delbtn");    
-    // define text input
-    const editInput = document.createElement("input");
-    editInput.classList.add("edittext");
-    editInput.type = "text";
-    editInput.value = label.textContent;
-    editInput.placeholder = label.textContent;
-
-    nodeSwap(parent,[[editInput,label],[acceptEdit,editBtn],[cancelBtn,delBtn]]);
-    // event listeners for accept changes and cancel changes
-    const nodeArr = [[label,editInput],[editBtn,acceptEdit],[delBtn,cancelBtn]];
-    acceptEdit.addEventListener("click", e => {
-        closeEditor(true,nodeArr);
-    });
-    cancelBtn.addEventListener("click", e => {
-        closeEditor(false,nodeArr);
-    })
-}
-
-function closeEditor(acceptChanges,nodeArr) {
-    const parent = event.target.parentElement;  
-    if (acceptChanges) nodeArr[0][0].textContent = nodeArr[0][1].value;
-    else nodeArr[0][0].textContent = nodeArr[0][1].placeholder;
-    nodeSwap(parent,nodeArr);
-}
-
-function nodeSwap(parent,nodeArr) {
-    nodeArr.forEach(e => parent.replaceChild(e[0],e[1]));
-}
+    // creates text editor buttons gives them some preset attributes
+    const acceptEdit = createButton("&#10003", "btn");  
+    const cancelBtn = createButton("&#10060", "btn");
+    // creates text input field gives it some preset attributes
+    const editInput = createTextInput(label.textContent);  
+    // swaps new editor related elements on to page
+    nodeSwap(parent, [
+      [editInput, label],
+      [acceptEdit, editBtn],
+      [cancelBtn, delBtn]
+    ]);
+  
+    acceptEdit.addEventListener("click", () => closeEditor(true, parent, [[label, editInput], [editBtn, acceptEdit], [delBtn, cancelBtn]]));
+    cancelBtn.addEventListener("click", () => closeEditor(false, parent, [[label, editInput], [editBtn, acceptEdit], [delBtn, cancelBtn]]));
+  }
+  
+  function closeEditor(acceptChanges, parent, nodeArr) {
+    const [originalNode, newNode] = nodeArr[0];
+    if (acceptChanges) originalNode.textContent = newNode.value;
+    else originalNode.textContent = newNode.placeholder;
+    nodeSwap(parent, nodeArr);
+  }
+  
+  function nodeSwap(parent, nodeArr) {
+    nodeArr.forEach(([newNode, oldNode]) => parent.replaceChild(newNode, oldNode));
+  }
+  
+  function createButton(text, className) {  // this and one below should be abstracted in to a single helper file for the whole project ideally
+    const button = document.createElement("button");
+    button.innerHTML = text;
+    button.classList.add(className);
+    return button;
+  }
+  
+  function createTextInput(value) {
+    const input = document.createElement("input");
+    input.classList.add("edittext");
+    input.type = "text";
+    input.value = value;
+    input.placeholder = value;
+    return input;
+  }
