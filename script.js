19878e024c19af8e68af1319b8bab63af96532f0
JamesESS <60142087+JamesESS@users.noreply.github.com>
2023-06-30 13:06:33 +0100

    restore script

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import { addEventListener } from "./JS/addEventListener.js";
 import { removeTask } from "./JS/deleteTask.js";
 import { editTask } from "./JS/editTask.js";
-
 addEventListener();
 
 setInterval(removeTask,1000);  // Need to replace this with calling remove task every time new item is added. Or better pass a const with all delbtns to removeTask
