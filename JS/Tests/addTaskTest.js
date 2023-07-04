d0d9cb189bb6a01292e9e0829f5ca456a8a4f9ef
JamesESS <60142087+JamesESS@users.noreply.github.com>
2023-07-04 11:41:43 +0100

    added tests for date and task type moved function to set date input to todays date to end of addTaskTest changed addTaskTest to use simulated click instead of event dispatch'submit'

--- JS/Tests/addTaskTest.js
+++ JS/Tests/addTaskTest.js
@@ -53,4 +53,24 @@ export const addTaskTest = () => {
       `Date Expected Due: ${due} Recieved: ${dueDate}`
     );
   });
+  dateReset();
 };
+
+
+function dateReset() {// set date selector value and min value to todays date
+  const dateSelector = document.getElementById("due-date");
+  const dateValue = todaysDate();
+  dateSelector.value = dateValue;
+  dateSelector.min = dateValue;
+}
+
+function todaysDate() {
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  let mm = today.getMonth() + 1; // Months start at 0!
+  let dd = today.getDate(); 
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+  const formattedToday = `${yyyy}-${mm}-${dd}`;
+  return formattedToday
+}
