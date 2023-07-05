7189fe839e7ea18dfbe8dbe9b509bfd2ea49b02c
JamesESS <60142087+JamesESS@users.noreply.github.com>
2023-07-05 13:38:30 +0100

    Added functionality to all, pending & completed. Currently in it's own JS need to integrated with modular JS Need to integrate home & work filters

---
+++ JS/filterTasks.js
@@ -0,0 +1,40 @@
+const showAllBtn = document.getElementById("all");
+const showPendingBtn = document.getElementById("pending");
+const showCompletedBtn = document.getElementById("completed");
+const taskContainer = document.getElementById("task-list");
+
+showAllBtn.addEventListener("click", e => {
+    activeButtonFunc(e);
+    showAllFunc();
+});
+
+showPendingBtn.addEventListener("click", e => {
+    activeButtonFunc(e);
+    filterTasksFunc(true);
+});
+
+showCompletedBtn.addEventListener("click", e => {
+    activeButtonFunc(e);
+    filterTasksFunc(false);
+});
+
+function activeButtonFunc(e) {
+    showAllBtn.classList.remove("active");
+    showPendingBtn.classList.remove("active");
+    showCompletedBtn.classList.remove("active");
+    e.target.classList.add("active");
+}
+
+function showAllFunc() {
+    const tasks = [...taskContainer.childNodes].filter(parent => parent.tagName == "DIV");
+    [...tasks].forEach(task => task.style.display = "flex");
+}
+
+function filterTasksFunc(showPending) {
+    showAllFunc();
+    const tasks = [...taskContainer.childNodes].filter(parent => parent.tagName == "DIV");
+    console.log(tasks);
+    const completedTasks = [...tasks].filter((task) => task.childNodes[0].checked == showPending)
+    console.log(completedTasks);
+    [...completedTasks].forEach(task => task.style.display = "none");
+}
