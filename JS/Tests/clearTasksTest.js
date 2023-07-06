368f3de5999e532dafb24139d0a866db629b75d2
DeepsDali <deepashri.dali@gmail.com>
2023-07-06 13:37:54 +0100

    Define variable to check if toggleemptymessage function called
    Relates #30

--- JS/Tests/clearTasksTest.js
+++ JS/Tests/clearTasksTest.js
@@ -1,3 +1,10 @@
+import { clearCompletedTasks, clearAllTasks } from "../clearTasks.js";
+import { toggleEmptyMessage } from "../Helpers/toggleEmptyMessage.js";
+
 export const clearTasksTest = () => {
   console.log("%cCLEAR TASK TESTS", "font-weight: bold; color:red");
+  let isToggleEmptyMessageCalled = false;
+  if (toggleEmptyMessage) {
+    isToggleEmptyMessageCalled = true;
+  }
 };
