0a1c0986af166008c96245768f4ded5c1f02c1ac
DeepsDali <deepashri.dali@gmail.com>
2023-07-06 09:20:25 +0100

    Use toggle message helper in addtask function
    Relates #28

--- JS/addTask.js
+++ JS/addTask.js
@@ -1,4 +1,5 @@
 import { getDisplayDate } from "./Helpers/getDisplayDate.js";
+import { toggleEmptyMessage } from "./Helpers/toggleEmptyMessage.js";
 
 export const addTask = () => {
   const taskList = document.getElementById("task-list");
@@ -48,12 +49,8 @@ export const addTask = () => {
 
   // Append the task item div to the task list
   taskList.appendChild(taskItem);
-  const message = document.getElementById("message");
-  if (taskList.children.length > 0) {
-    message.style.display = "none";
-  } else {
-    message.style.display = "block";
-  }
+
+  toggleEmptyMessage();
 
   // Clear the input field
   document.getElementById("addTask").value = "";
